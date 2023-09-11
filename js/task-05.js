3814160f610b55fa8cc6ad51e05481d69b0506dc
Nazarii-Ivan Stosyk <fantombeowulf@gmail.com>
2023-09-12 00:49:02 +0300

    HW 6 task 5 is done

---
+++ js/task-05.js
@@ -0,0 +1,12 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output')
+// console.log(inputEl)
+
+inputEl.addEventListener("input", (event) => {
+    const inputValue = inputEl.value;
+    if (inputValue === '') {
+    outputEl.textContent = "Anonymous"
+    } else {
+        outputEl.textContent = event.currentTarget.value;
+}
+})
