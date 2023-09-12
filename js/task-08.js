202f87e7fa6d5094cfbfb6b7897d1859abfa0b49
Nazarii-Ivan Stosyk <fantombeowulf@gmail.com>
2023-09-12 03:26:04 +0300

    hw 6 task 8 is done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (event.currentTarget.email.value === '' || event.currentTarget.password.value === '') {
+        alert('Всі поля повинні бути заповнені!');
+    }
+    const { email, password } = event.currentTarget.elements;
+    const info = {
+        email: email.value,
+        password:password.value,
+    }
+    console.log(info);
+    formEl.reset();
+})
 
